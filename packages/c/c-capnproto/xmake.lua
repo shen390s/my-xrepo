daa9eccf8ebff947e784de4f9fa02f28fef47911
Rongsong Shen <rongsong.shen@akrostar-tech.com>
2025-05-15 16:32:08 +0800

    minor changes

--- packages/c/c-capnproto/xmake.lua
+++ packages/c/c-capnproto/xmake.lua
@@ -8,11 +8,13 @@ package("c-capnproto")
 
     add_links("capnpc-c")
 
+    add_deps("cmake")
+
     on_install(function (package)
         local configs = {"-DBUILD_TESTING=OFF"}
 	table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
         table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-        import("package.tools.xmake").install(package)
+        import("package.tools.cmake").install(package)
 	package:addenv("PATH", "bin")
     end)
 
