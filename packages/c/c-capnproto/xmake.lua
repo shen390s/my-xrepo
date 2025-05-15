c7205655ddeb73867ac571dea6a2de4ce91049ce
Rongsong Shen <rongsong.shen@akrostar-tech.com>
2025-05-15 16:22:39 +0800

    minor changes

--- packages/c/c-capnproto/xmake.lua
+++ packages/c/c-capnproto/xmake.lua
@@ -1,20 +1,15 @@
 package("c-capnproto")
     set_homepage("https://github.com/shen390s/c-capnproto")
-    set_description("")
+    set_description("capnproto plugin to generate c ")
     set_license("MIT")
 
     add_urls("https://github.com/shen390s/c-capnproto.git")
     add_versions("1.1.0", "a86c5ab151d008570eb7321416a0a46dd6e19131")
 
+    add_links("capnpc-c")
+
     on_install(function (package)
         local configs = {}
-        io.writefile("xmake.lua", [[
-            add_rules("mode.release", "mode.debug")
-            target("c-capnproto")
-                set_kind("")
-                add_files("src/*.c")
-                add_headerfiles("src/(*.h)")
-        ]])
         import("package.tools.xmake").install(package)
     end)
 
