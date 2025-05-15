8b5d8f79e5ff1b53e467ce79b23960e834bcec58
Rongsong Shen <rongsong.shen@akrostar-tech.com>
2025-05-15 16:45:48 +0800

    minor changes

--- packages/c/c-capnproto/xmake.lua
+++ packages/c/c-capnproto/xmake.lua
@@ -6,7 +6,7 @@ package("c-capnproto")
     add_urls("https://github.com/shen390s/c-capnproto.git")
     add_versions("1.1.0", "a86c5ab151d008570eb7321416a0a46dd6e19131")
 
-    add_links("capnpc-c")
+--    add_links("capnpc-c")
 
     add_deps("cmake")
 
