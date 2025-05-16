4a609fc9caeca71ee74445717593a41ad416a476
Rongsong Shen <rongsong.shen@akrostar-tech.com>
2025-05-16 17:10:12 +0800

    add elogind

---
+++ packages/e/elogind/xmake.lua
@@ -0,0 +1,21 @@
+package("elogind")
+    set_homepage("https://github.com/elogind/elogind")
+    set_description("The systemd project's "logind", extracted to a standalone package")
+    set_license("GPL-2.0")
+
+    add_urls("https://github.com/elogind/elogind/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/elogind/elogind.git")
+
+    add_versions("v255.17", "a9725ae3f73f8d910de84c108bc11bfd4c782bef6a4190b2ec70c5d2f22344db")
+
+    add_deps("autoconf", "automake", "libtool")
+
+    on_install(function (package)
+        local configs = {}
+        table.insert(configs, "--enable-shared=" .. (package:config("shared") and "yes" or "no"))
+        if package:is_debug() then
+            table.insert(configs, "--enable-debug")
+        end
+        import("package.tools.autoconf").install(package, configs)
+    end)
+
