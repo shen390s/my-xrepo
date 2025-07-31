efe38b97be6169fa423ce72da9a5f5c409282a82
Rongsong Shen <rongsong.shen@gmail.com>
2025-07-31 11:16:56 +0800

    add Cello

---
+++ packages/c/cello/xmake.lua
@@ -0,0 +1,24 @@
+package("cello")
+    set_homepage("http://libcello.org/")
+    set_description("Higher level programming in C")
+
+    add_urls("https://github.com/orangeduck/Cello/archive/refs/tags/$(version).tar.gz",
+             "https://github.com/orangeduck/Cello.git")
+
+    add_versions("v2.0.3", "1afcae06f5efc10ea161737a862073ff5679c964540bca7cd719539609d0633c")
+
+    on_install(function (package)
+        local configs = {}
+        io.writefile("xmake.lua", [[
+            add_rules("mode.release", "mode.debug")
+            target("cello")
+                set_kind("")
+                add_files("src/*.c")
+                add_headerfiles("src/(*.h)")
+        ]])
+        import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cfuncs("foo", {includes = "foo.h"}))
+    end)
